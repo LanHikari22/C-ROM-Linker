9c3e452bb67b50c0d15ac470c39d63ed2ad5874c
Alzakariya <alzakariyamq@ad.msoe.edu>
2017-11-01 11:44:32 -0500

    Implemented Symbol Table Parsing and GetObjSymbols.bat

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,59 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.printf("And so... It has %s\n", "started!");
+import java.io.*;
+import java.util.Scanner;
 
+public class Main {
+
+    /**
+     * TODO: doc main()
+     *
+     * @param args Format: {ROM Path} {Path of Object File To Inject}
+     */
+    public static void main(String[] args) {
+        String objFile = "C:\\Users\\alzakariyamq\\Development\\ARM\\new.o";
+//        String type = "RELOCATION_TABLE";
+        String type = "SYMBOL_TABLE";
+        try {
+            String output = runBatScript("GetObjSymbols " + objFile + " " + type);
+            SymbolTable symTbl = new SymbolTable(output);
+
+        } catch(IOException e){
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    /**
+     * Executes a batch script and returns the output of it as a string.
+     * One simple rule. The first two lines must be "\n" and "echo off".
+     * Unexpected behavior may occur otherwise.
+     * @param batScript The path to the batch script to run
+     * @throws IOException Errors that arise through the runtime environment, or through errors in the batch file.
+     * @return The output of the script.
+     */
+    private static String runBatScript(String batScript) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        Process p = Runtime.getRuntime().exec("cmd /c \"\" " + batScript);
+        Scanner s = new Scanner(p.getInputStream());
+
+        // first two lines are "\n" and "echo off".
+        if(s.hasNextLine()) s.nextLine();
+        else System.err.println("Invalid batch file format");
+        if(s.hasNextLine()) s.nextLine();
+        else System.err.println("Invalid batch file format");
+
+        while(s.hasNextLine()) {
+            sb.append(s.nextLine() + "\n");
+        }
+
+        // If there are any errors, throw an IOException indicating
+        s = new Scanner(p.getErrorStream());
+        if(s.hasNextLine()){
+            StringBuilder sb_error = new StringBuilder();
+            sb_error.append("Encountered Errors while running batch file:\n");
+            while(s.hasNextLine())
+                sb_error.append((s.nextLine() + "\n"));
+            throw new IOException(sb_error.toString());
+        }
+        return sb.toString();
     }
 }
--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -1,2 +1,70 @@
+import java.util.Scanner;
+
 public class SymbolTable {
+
+    /**
+     * An Entry object represents one row in the symbol table.
+     */
+    static class Entry {
+        int Num;
+        int Value;
+        int Size;
+        String Type;
+        String Bind;
+        String Vis;
+        int Ndx;
+        String Name;
+
+        static final int Ndx_UND = -1;
+        static final int Ndx_ABS = -2;
+    }
+
+    /**
+     * Extracted Symbol Table
+     */
+    public Entry[] Table;
+
+    public SymbolTable(String symTableReport){
+        Table = parse(symTableReport);
+    }
+
+    private static Entry[] parse(String symTableReport) {
+        Scanner s = new Scanner(symTableReport);
+        // ignore initial line
+        s.nextLine();
+        // extract size from second line
+        for(int i=0;i<4;i++) s.next();
+        int size = s.nextInt();
+        s.nextLine();
+        // initialize table output
+        Entry[] output = new Entry[size];
+        // Next line shows labels of entrys, not an entry.
+        s.nextLine();
+        // Now parse the next size entries into the output table
+        for(int i = 0; i < size; i++) {
+            Entry entry = new Entry();
+            // parse Num
+            String temp = s.next();
+            entry.Num = Integer.parseInt(temp.substring(0,temp.length()-1));
+//            System.out.print(entry.Num + ": ");
+            // parse Value, Size, Type, Bind, and Vis
+            entry.Value = s.nextInt(16);
+            entry.Size = s.nextInt();
+            entry.Type = s.next();
+            entry.Bind = s.next();
+            entry.Vis = s.next();
+//            System.out.printf("%08x, %d, %s, %s, %s", entry.Value, entry.Size, entry.Type, entry.Bind, entry.Vis);
+            // parse Ndx
+            temp = s.next();
+            if(temp.equals("UND")) entry.Ndx = Entry.Ndx_UND;
+            else if(temp.equals("ABS")) entry.Ndx = Entry.Ndx_ABS;
+            else entry.Ndx = Integer.parseInt(temp);
+//            System.out.print(", " + entry.Ndx);
+            // parse Name
+            entry.Name = s.nextLine();
+//            System.out.println(", " + entry.Name);
+        }
+//        System.out.println(symTableReport);
+        return output;
+    }
 }
